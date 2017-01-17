146735d8aaebe9852d18dcf5a1235acbf488313e
timellemeet <tim@ellemeet.net>
2017-01-18 00:46:20 +0100

    fixed missing twitter accounts

--- index.js
+++ index.js
@@ -45,8 +45,11 @@ fs.createReadStream(__dirname+'/data.csv')
 
 function parse_people(data){
   let people = [];
-  data.forEach(person => people.push({name: person[0],
-            twitter:person[6].substring(person[6].lastIndexOf("/")+1)}))
+  data.forEach(person => {
+    if(person[6]){
+    people.push({name: person[0], twitter:person[6].substring(person[6].lastIndexOf("/")+1)})
+  }
+  })
   return people
 }
 
