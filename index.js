c83ec94cac89d20d92239d0acbf248d53e009eed
timellemeet <tim@ellemeet.net>
2017-01-17 23:47:59 +0100

    added multifetch

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const request = require('request-promise')
+const parse = require('csv-parse')
+const fs = require('fs')
 
 //request options
 function options(screenname) {
@@ -17,11 +19,42 @@ function options(screenname) {
   return options
 }
 
+//csv parse function
+const parser = parse({delimter: ':'}, function(err,data){
+  let people = parse_people(data)
+  fetch_receptivity(people)
+})
 
+//reading the data csv file
+fs.createReadStream(__dirname+'/data.csv')
+  .pipe(parser)
 
+function parse_people(data){
+  let people = [];
+  data.forEach(person => people.push({name: person[0],
+            twitter:person[6].substring(person[6].lastIndexOf("/")+1)}))
+  return people
+}
+
+function fetch_receptivity(people){
+  let fetches = []
+  people.forEach(person => fetches.push(request(options(person.twitter))))
+
+  Promise.all(fetches).then(values => {
+    let result = [];
+    for(let i = 0; i<people.length;i++){
+      result.push({name: people[i], data: values[i]})
+    }
+    return result;
+  })
+  .then(data => console.log(data))
+}
+
+/*
 request(options('realDonaldTrump'), function (error, response, body) {
   if (error) throw new Error(error);
 
   console.log(body);
 });
 
+*/
