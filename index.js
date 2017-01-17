64717fa21151e166a6389e3e025a5bcbd21bb23d
timellemeet <tim@ellemeet.net>
2017-01-18 00:01:08 +0100

    updated tokens

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ function options(screenname) {
   let options = { method: 'POST',
   url: 'https://app.receptiviti.com/v2/api/import/twitter/user',
   headers:
-   { 'postman-token': 'f7709fd5-462e-abb9-9a16-2bf78301be37',
+   {
      'cache-control': 'no-cache',
-     'x-api-secret-key': 'pufoerD5nYKA1F7dBtqsDhe98JRUiQPm5w9L7BkO2CM',
-     'x-api-key': '587e8da9ddf37305920b849a',
+     'x-api-secret-key': 'I0ICSjb8LasMgHgj9X96tEzJgkifi1zsprAuhAG2cZE',
+     'x-api-key': '58739637412cee05ed8fd5d8',
      'content-type': 'application/json' },
   json: true };
 
@@ -50,11 +50,3 @@ function fetch_receptivity(people){
   .then(data => console.log(data))
 }
 
-/*
-request(options('realDonaldTrump'), function (error, response, body) {
-  if (error) throw new Error(error);
-
-  console.log(body);
-});
-
-*/
